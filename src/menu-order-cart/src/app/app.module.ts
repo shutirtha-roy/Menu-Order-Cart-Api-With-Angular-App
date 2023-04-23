df7b5b0e4246304dfeef30fc45f176a5344c627e
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-04-23 13:26:18 +0600

    Header Added

--- src/menu-order-cart/src/app/app.module.ts
+++ src/menu-order-cart/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { NotfoundComponent } from './shared/components/notfound/notfound.component';
+import { PagesComponent } from './pages/pages.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HeaderComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NgbModule
   ],
--- src/menu-order-cart/src/app/pages/pages.module.ts
+++ src/menu-order-cart/src/app/pages/pages.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
-import { PagesComponent } from './pages.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    PagesComponent
+    
   ],
   imports: [
     CommonModule,
     PagesRoutingModule,
-    BrowserAnimationsModule,
     ToastrModule.forRoot()
   ]
 })
