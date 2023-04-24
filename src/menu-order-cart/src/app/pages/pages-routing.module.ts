dc5d5f5e62193ee204672cb841a6000c53d3f576
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-04-24 18:34:30 +0600

    Default routing set to menu

--- src/menu-order-cart/src/app/pages/pages-routing.module.ts
+++ src/menu-order-cart/src/app/pages/pages-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'menu',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     children: [
       {
         path: 'menu',
