78271f4c28d717b21eaab0a7a9b7c21a863e8119
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-05-12 23:27:08 +0600

    data added

--- src/menu-order-cart/src/app/data/menu.ts
+++ src/menu-order-cart/src/app/data/menu.ts
@@ -28,5 +28,55 @@
                 price: 7.99
             },
         ]
+    },
+    {
+        "category": "Entrees",
+        "menu": [
+            {
+                id: 1,
+                name: "Spaghetti and Meatballs",
+                price: 12.99 
+            },
+            {
+                id: 1,
+                name: "Chicken Parmesan",
+                price: 14.99 
+            },
+            {
+                id: 1,
+                name: "Shrimp Scampi",
+                price: 16.99 
+            },
+            {
+                id: 1,
+                name: "Vegetable Stir-Fry",
+                price: 10.99 
+            },
+        ]
+    },
+    {
+        "category": "Beverages",
+        "menu": [
+            {
+                id: 1,
+                name: "Coke",
+                price: 1.99 
+            },
+            {
+                id: 2,
+                name: "Diet Coke",
+                price: 1.99 
+            },
+            {
+                id: 3,
+                name: "Sprite",
+                price: 1.99 
+            },
+            {
+                id: 4,
+                name: "Lemonade",
+                price: 2.99 
+            },
+        ]        
     }
 ]
