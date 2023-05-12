9e37f3665c31f3debe4c6a356dea5a403acd9d60
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-05-12 23:15:23 +0600

    Menu data modified

---
+++ src/menu-order-cart/src/app/data/menu.ts
@@ -0,0 +1,32 @@
+[
+    {
+        "category": "Appetizers",
+        "menu": [
+            {
+                id: 1,
+                name: "Bruschetta",
+                price: 6.99 
+            },
+            {
+                id: 2,
+                name: "Caprese Salad",
+                price: 8.99
+            },
+            {
+                id: 3,
+                name: "Garlic Knots",
+                price: 4.99
+            },
+            {
+                id: 4,
+                name: "Garlic Knots",
+                price: 4.99
+            },
+            {
+                id: 5,
+                name: "Mozzarella Sticks",
+                price: 7.99
+            },
+        ]
+    }
+]
